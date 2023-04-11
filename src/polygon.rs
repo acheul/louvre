7817383435734665b5a85a2bc1e26dfd8a4fdfcf
Acheul <seoul605.21@gmail.com>
2023-04-11 11:20:21 +0900

    filechg

--- src/polygon.rs
+++ src/polygon.rs
@@ -1,2 +1,2 @@
 pub mod triangulation;
-mod utils;
+pub mod utils;
--- src/polygon/triangulation.rs
+++ src/polygon/triangulation.rs
@@ -14,9 +14,9 @@ use std::f64;
 /// Each Vertices are linked to their own previous and next vertices.
 /// The linking is implemented via the raw pointer of Rust. Hopefully, it's designed to be memory safe.
 pub struct Vertex<'a> {
-  i: usize,
-  x: f64,
-  y: f64,
+  pub i: usize,
+  pub x: f64,
+  pub y: f64,
   topdown: bool,
   top: f64,
   bottom: f64,
@@ -24,10 +24,10 @@ pub struct Vertex<'a> {
   right: f64,
   sign: bool,
   valid: bool,
-  sects: Option<Vec<*mut Sect<'a>>>,
+  pub sects: Option<Vec<*mut Sect<'a>>>,
   prev: *mut Vertex<'a>,
   next: *mut Vertex<'a>,
-  next_sect: *mut Sect<'a>,
+  pub next_sect: *mut Sect<'a>,
 }
 
 /// The Sect struct handles points generated from intersection between original segments.
@@ -36,8 +36,8 @@ pub struct Sect<'a> {
   x: f64,
   y: f64,
   dual: *mut Sect<'a>,
-  next: *mut Sect<'a>,
-  other: *mut Vertex<'a>,
+  pub next: *mut Sect<'a>,
+  pub other: *mut Vertex<'a>,
   sign: bool,
   valid: bool,
 }
@@ -621,7 +621,7 @@ fn decomp_remain_sects(s: &mut *mut Sect, i: &mut usize, new_data: &mut Vec<f64>
 
 
 
-fn top_turn(v: &*mut Vertex) -> bool {
+pub fn top_turn(v: &*mut Vertex) -> bool {
   unsafe {
     let mut v_prev = (*(*v)).prev;
     let mut v_next = (*(*v)).next;
@@ -846,7 +846,7 @@ pub fn update_intersect(array: &Vec<*mut Vertex>) -> bool {
   if count>0 { true } else { false }
 }
 
-fn intersect(x1:f64,y1:f64, x2:f64,y2:f64, x3:f64,y3:f64, x4:f64,y4:f64)
+pub fn intersect(x1:f64,y1:f64, x2:f64,y2:f64, x3:f64,y3:f64, x4:f64,y4:f64)
  -> Option<(f64, f64, f64, f64)> {
 
   let denominator = (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4);
@@ -956,249 +956,4 @@ fn fill_linked_vertex_array(order: bool, data: &mut Vec<f64>, dim: usize) -> Vec
     }
   }
   array
-}
-
-
-
-// ----------- //
-// -- TESTs -- //
-// ----------- //
-
-mod test {
-  #[test]
-  fn test_signed_area() {
-    use super::signed_area;
-    use super::Winding;
-    let data: Vec<f64> = vec![0.,0., 2.,0., 1.,1.];
-    let result = signed_area(&data, 2);
-    assert_eq!(result, Winding::CCW);
-
-    let data: Vec<f64> = vec![0.,0., 1.,1., 2.,0.];
-    let result = signed_area(&data, 2);
-    assert_eq!(result, Winding::CW);
-
-    let data: Vec<f64> = vec![0.,0., 2.,2., 1.,1.];
-    let result = signed_area(&data, 2);
-    assert_eq!(result, Winding::Zero);
-  }
-
-  #[test]
-  fn test_area() {
-    use super::area;
-    use super::Winding;
-    let result = area(0.,0., 2.,0., 1.,1.);
-    assert_eq!(result, Winding::CCW);
-
-    let result = area(0.,0., 1.,1., 2.,0.);
-    assert_eq!(result, Winding::CW);
-
-    let result = area(0.,0., 2.,2., 1.,1.);
-    assert_eq!(result, Winding::Zero);
-  }
-
-  #[test]
-  fn test_linked_vertex_array() {
-    use super::linked_vertex_array;
-    use super::Vertex;
-    fn do_test(data: &mut Vec<f64>, dim: usize) -> (Vec<f64>, Vec<usize>) {
-      let mut array: Vec<*mut Vertex> = linked_vertex_array(data, dim);
-      let mut v1 = Vec::new(); // push floats;
-      let mut v2 = Vec::new(); // push indices;
-      unsafe {
-        for a in array.iter_mut() {
-          v1.push((*(*a)).x.clone());
-          v1.push((*(*a)).y.clone());
-          v2.push((*(*a)).i.clone());
-          drop(Box::from_raw(*a)); // consume
-        }
-      }
-      return (v1, v2);
-    }
-    let mut data = vec![0.,0., 1.,1., 2.,0., 0.,0., 0.,0.];
-    let (v1, v2) = do_test(&mut data, 2);
-    assert_eq!(v1, vec![2.,0., 1.,1., 0.,0.]);
-    assert_eq!(v2, vec![0, 1, 2]);
-
-    let mut data = vec![0.,0., 2.,0., 1.,1., 0.,0., 0.,0.];
-    let (v1, v2) = do_test(&mut data, 2);
-    assert_eq!(v1, vec![0.,0., 2.,0., 1.,1.]);
-    assert_eq!(v2, vec![0, 1, 2]);
-
-    let mut data = vec![0.,0., 1.,1.];
-    let (v1, v2) = do_test(&mut data, 2);
-    assert_eq!(v1, vec![0.,0., 1.,1.]);
-    assert_eq!(v2, vec![0, 1,]);
-
-    let mut data = vec![0.,0., 0.,0.];
-    let (v1, v2) = do_test(&mut data, 2);
-    assert_eq!(v1, vec![]);
-    assert_eq!(v2, vec![]);
-
-    let mut data = vec![0.,0.];
-    let (v1, v2) = do_test(&mut data, 2);
-    assert_eq!(v1, vec![]);
-    assert_eq!(v2, vec![]);
-  }
-
-  #[test]
-  fn test_sort_by() {
-    use super::linked_vertex_array;
-    use super::Vertex;
-    fn do_test(data: &mut Vec<f64>) -> (Vec<f64>, Vec<usize>) {
-      let mut array = linked_vertex_array(data, 2);
-      let mut v1: Vec<f64> = Vec::new();
-      let mut v2: Vec<usize> = Vec::new();
-      unsafe {
-        array.sort_by(|b, a| (&(*(*a))).partial_cmp(&(*(*b))).unwrap());
-        for a in array.iter() {
-          v1.push((*(*a)).x);
-          v1.push((*(*a)).y);
-          v2.push((*(*a)).i);
-          drop(Box::from_raw(*a));
-        }
-        return (v1, v2)
-      }
-    }
-    let mut data = vec![0.,0., 2.,0., 2.,0., 2.,2., 0.,2.,];
-    let (v1, v2) = do_test(&mut data);
-    assert_eq!(v1, vec![0.,2., 2.,2., 0.,0., 2.,0., 2.,0.,]);
-    assert_eq!(v2, vec![4, 3, 0, 1, 2]);
-  }
-
-  #[test]
-  fn test_intersect() {
-    use super::intersect;
-    assert_eq!(Some((1.,0.,0.5,0.5)), intersect(0.,0., 2.,0., 1.,-1., 1.,1.));
-  }
-
-  #[test]
-  fn test_top_turn() {
-    use super::{linked_vertex_array, top_turn};
-
-    fn do_test(data: &mut Vec<f64>) -> bool {
-      let mut array = linked_vertex_array(data, 2);
-      unsafe { array.sort_by(|b, a| (&(*(*a))).partial_cmp(&(*(*b))).unwrap()); }
-      let sign = top_turn(&(array[0]));
-      // consume raw pointers
-      unsafe {
-        for a in array.iter() {
-          if let Some(sects) = &(*(*a)).sects {
-            for s in sects.iter() {
-              drop(Box::from_raw(*s));
-            }
-          }
-          drop(Box::from_raw(*a));
-        }
-      }
-      sign
-    }
-
-    let mut data = vec![-1.,0., -1.,-1., 1.,1., 1.,0.];
-    let sign = do_test(&mut data);
-    assert_eq!(sign, false);
-    
-    let mut data = vec![1.,1., 1.,1., -1.,1., -1.,1., -1.,1., 0.,0., 1.,0.];
-    let sign = do_test(&mut data);
-    assert_eq!(sign, true);
-
-    let mut data = vec![1.,1., 1.,1., -1.,1., -1.,1., -2.,1., -1.,1., 0.,0., 1.,0.];
-    let sign = do_test(&mut data);
-    assert_eq!(sign, true);
-
-    let mut data = vec![-2.,1., -2.,1., -1.,1., -1.,1., -1.,1., 1.,1., 1.,0., 0.,0.,];
-    let sign = do_test(&mut data);
-    assert_eq!(sign, true);
-
-    let mut data = vec![-1.,1., -1.,1., -1.,1., 1.,1., 1.,1., -2.,-2., 2.,-2.,];
-    let sign = do_test(&mut data);
-    assert_eq!(sign, false);
-
-    let mut data = vec![-1.,1., -1.,1., -1.,1., 1.,1.,];
-    let sign = do_test(&mut data);
-    assert_eq!(sign, true);
-  }
-
-  #[test]
-  
-  fn test_update_sects() {
-    use super::{linked_vertex_array, update_intersect, update_sects, Vertex};
-    unsafe {
-      let mut data = vec![-1.,0., -1.,-1., 1.,1., 1.,0.];
-      let mut array = linked_vertex_array(&mut data, 2);
-      array.sort_by(|b, a| (&(*(*a))).partial_cmp(&(*(*b))).unwrap());
-      if update_intersect(&mut array) {
-        update_sects(&mut array[0]);
-
-        for a in array.iter() {
-          if (*(*a)).x==-1. && (*(*a)).y==-1. {
-            if let Some(sects) = &(*(*a)).sects {
-              assert_eq!(sects.len(), 1);
-              // assert_eq!(sects.len(), 2); // Must be an error;
-            }
-          }
-        }
-      }
-      unsafe {
-        for a in array.iter() {
-          if let Some(sects) = &(*(*a)).sects {
-            for s in sects.iter() {
-              drop(Box::from_raw(*s));
-            }
-          }
-          drop(Box::from_raw(*a));
-        }
-      }
-
-      let mut data = vec![-2.,-2., 2.,2., 1.,-2., -1.,2., 1.,2., -1.,-2., -2.,1., 2.,-1., 2.,0., -2.,0.,]; // 반대방향으로 연결됨.
-      let mut array = linked_vertex_array(&mut data, 2);
-      array.sort_by(|b, a| (&(*(*a))).partial_cmp(&(*(*b))).unwrap());
-      if update_intersect(&mut array) {
-        update_sects(&mut array[0]);
-
-        for a in array.iter() {
-          if (*(*a)).x==2. && (*(*a)).y==2. {
-            if let Some(sects) = &(*(*a)).sects {
-              //assert_eq!(sects.len(), 5);
-              // assert_eq!(sects.len(), 2); // Must be an error;
-
-              let mut t = 0;
-              for s in sects.iter() {
-                if ! (*(*s)).next.is_null() { t+=1; }
-              }
-              // assert_eq!(t, 1);
-              // assert_eq!(i, 2); // Must be an error;
-            }
-            if ! (*(*a)).next_sect.is_null() {
-              let x = (*(*(*(*a)).next_sect).other).x;
-              let y = (*(*(*(*a)).next_sect).other).y;
-              assert_eq!(x, -2.);
-              assert_eq!(y, 0.);
-            }
-          }
-        }
-      }
-      unsafe {
-        for a in array.iter() {
-          if let Some(sects) = &(*(*a)).sects {
-            for s in sects.iter() {
-              drop(Box::from_raw(*s));
-            }
-          }
-          drop(Box::from_raw(*a));
-        }
-      }
-    }
-  }
-  
-
-
-  #[test]
-  fn test_triangulate() {
-    use super::triangulate;
-    let mut data = vec![0.,0., 2.,0., 2.,0., 2.,2., 0.,2.,];
-    let mut data = vec![-1.,0., -1.,-1., 1.,1., 1.,0.];
-    let mut data = vec![-2.,-2., 2.,2., 1.,-2., -1.,2., 1.,2., -1.,-2., -2.,1., 2.,-1., 2.,0., -2.,0.,];
-    let result = triangulate(&mut data, 2);
-    // assert_eq!(result, (vec![1.0, 2.0], vec![1, 2])); // Would fail.
-  }
 }
